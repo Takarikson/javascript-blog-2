0f53aa4305c463772fcb798be75edc2bf867cfd9
Krzysztof Fila <kfila17@gmail.com>
2020-05-15 23:35:35 +0200

    add class active to clickedElement

--- js/script.js
+++ js/script.js
@@ -2,14 +2,15 @@
 
 function titleClickHandler(event) {
   console.log(event);
-  /* remove class 'active' from all article links  */
+  /* [DONE] remove class 'active' from all article links  */
   const activeLinks = document.querySelectorAll('.titles a.active');
   for (let activeLink of activeLinks) {
     activeLink.classList.remove('active');
   }
   /* add class 'active' to the clicked link */
-
-  /* remove class 'active' from all articles */
+  const clickedElement = this;
+  clickedElement.classList.add('active');
+  /* [DONE] remove class 'active' from all articles */
   const activeArticles = document.querySelectorAll('article');
   for (let activeArticle of activeArticles) {
     activeArticle.classList.remove('active');
